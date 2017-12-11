fd7a9c7cbd5b3e3a75c9111cd5e88369293bfa8b
Alexandr Sulimov <alexandr@ase.com.ua>
2017-12-11 16:16:10 +0200

    sound signal on js build

--- Web.MyOffice/gulpfile.js
+++ Web.MyOffice/gulpfile.js
@@ -2,19 +2,21 @@
 var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
 var watch = require('gulp-watch');
-//var gutil = require('gulp-util');
-//var batch = require('gulp-batch');
-//var jsValidate = require('gulp-jsvalidate');
 var eslint = require('gulp-eslint');
+var child_process = require('child_process');
 
 gulp.task('eslint', function () {
     return gulp.src(['app/**/*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+        .pipe(eslint.failAfterError())
+        .on('error', (error) => {
+            child_process.exec('powershell -c (New-Object Media.SoundPlayer \'..\\_Tools\\error.wav\').PlaySync();', (error, stdout, stderr) => { });
+        });
 });
 
 gulp.task("combine-and-uglify", ['eslint'], function () {
+    child_process.exec('powershell -c (New-Object Media.SoundPlayer \'..\\_Tools\\success.wav\').PlaySync();', (error, stdout, stderr) => { });
     return gulp.src('app/*.js')
         .pipe(concat('dis.min.js'))
         .pipe(uglify())
@@ -22,7 +24,7 @@ gulp.task("combine-and-uglify", ['eslint'], function () {
 });
 
 gulp.task("combine", ['eslint'], function () {
-
+    child_process.exec('powershell -c (New-Object Media.SoundPlayer \'..\\_Tools\\success.wav\').PlaySync();', (error, stdout, stderr) => { });
     gulp.src('app/**/*.js')
         .pipe(concat('dis.js'))
         .pipe(gulp.dest('scripts'));
