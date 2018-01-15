689ab45d39d2f8df5500e4250f49a051b6b9010a
Ruslan <ruslanshigimaga@gmail.com>
2018-01-15 21:59:40 +0200

    Layout editing.

--- Web.MyOffice/app/controllers/counterCtrl.js
+++ Web.MyOffice/app/controllers/counterCtrl.js
@@ -3,38 +3,13 @@
     angular.module('MyOffice.app').controller('counterCtrl', ['$scope', 'ModalWindowService', counterCtrl]);
 
     function counterCtrl($scope, ModalWindowService) {
-        $scope.casualNominals = [1, 2, 5, 10, 20, 50, 100];
-        $scope.selectedNominal = $scope.casualNominals.shift();
+        var nominalSearchKey = 'banknote';
+        var maxBanknoteNominals = 20;
+
+        $scope.nominalStorage = new NominalStorage(nominalSearchKey, maxBanknoteNominals);
+
         $scope.banknotes = [];
-        $scope.AddBanknote = function (nominal) {
-            if ($scope.banknotes.find(function (banknote) {
-                    return banknote.nominal === nominal;
-                }) ===
-                undefined && nominal !== undefined) {
-                var banknote = new Banknote(nominal);
-                $scope.banknotes.push(banknote);
-                $scope.banknotes.sort($scope.banknoteSorter);
-                $scope.getTotalMoney();
-                if ($scope.casualNominals.length !== 0) {
-                    $scope.selectedNominal = $scope.casualNominals.shift();
-                } else {
-                    $scope.selectedNominal = undefined;
-                }
-
-            }
-        };
-
-        $scope.DeleteBanknote = function (banknote) {
-            $scope.banknotes = $scope.banknotes.filter(function(_banknote) {
-                return _banknote.nominal !== banknote.nominal;
-            });
-            $scope.banknotes = $scope.banknotes.filter(function (_banknote) {
-                return _banknote.nominal !== banknote.nominal;
-            });
-            $scope.banknotes.sort($scope.banknoteSorter);
-            $scope.getTotalMoney();
-        };
-        $scope.banknoteSorter = function(prev, next) {
+        var banknoteSorter = function (prev, next) {
             if (prev.nominal > next.nominal) {
                 return 1;
             } else if (prev < next) {
@@ -43,6 +18,39 @@
                 return 0;
             }
         };
+        var nominalSorter = function (prev, next) {
+            if (prev > next) {
+                return 1;
+            } else if (prev < next) {
+                return -1;
+            } else {
+                return 0;
+            }
+        };
+
+        $scope.AddBanknote = function (nominal) {
+            var banknoteExists = $scope.banknotes.find(function (banknote) {
+                return banknote.nominal === nominal;
+            }) !== undefined;
+
+            if (!banknoteExists && nominal !== undefined) {
+                var banknote = new Banknote(nominal);
+                $scope.banknotes.push(banknote);
+                $scope.banknotes.sort(banknoteSorter);
+                $scope.getTotalMoney();
+                $scope.nominalStorage.add(nominal);
+                $scope.nominalStorage.nominals.sort(nominalSorter);
+            }
+        };
+        $scope.DeleteBanknote = function (banknote) {
+            $scope.banknotes = $scope.banknotes.filter(function(_banknote) {
+                return _banknote.nominal !== banknote.nominal;
+            });
+            $scope.banknotes.sort(banknoteSorter);
+            $scope.getTotalMoney();
+            $scope.nominalStorage.remove(banknote.nominal);
+            $scope.nominalStorage.nominals.sort(nominalSorter);
+        };
 
         $scope.getTotalMoney = function () {
             $scope.totalMoney = 0;
@@ -50,19 +58,86 @@
                 $scope.totalMoney += banknote.totalCost;
             });
         };
+        $scope.openDialog = function () {
+            ModalWindowService.open('CounterController', 'list', $scope, 'modal-sm');
+        };
+        $scope.closeDialog = function () {
+            ModalWindowService.close('list');
+        };
         function Banknote(nominal) {
             this.nominal = nominal;
             this.quantity = 1;
             this.totalCost = this.nominal * this.quantity;
             this.totalCostCalc = function () {
-                this.totalCost =  this.nominal * this.quantity;
+                this.totalCost = this.nominal * this.quantity;
                 $scope.getTotalMoney();
             };
         }
-        ModalWindowService.open('CounterController', 'list', $scope, 'modal-sm');
-        $scope.closeDialog = function () {
-            ModalWindowService.close('list');
-            history.back();
+    }
+
+
+
+    function NominalStorage(nominalSearchKey, maxBanknoteNominals) {
+        this.maxBanknoteNominals = maxBanknoteNominals;
+        this.nominalSearchKey = nominalSearchKey;
+        this.counter = -1;
+        this.nominals = null;
+
+        this.getAll = function () {
+            var vals = [];
+            for (var key in localStorage) {
+                if (key.indexOf(nominalSearchKey) !== -1) {
+                    vals.push(+localStorage[key]);
+                }
+            }
+            this.nominals = vals.sort();
+            return vals;
+        };
+        this.getAll();
+
+
+        this.add = function (nominal) {
+            if (this.nominals.indexOf(nominal) === -1) {
+                this.nominals.push(nominal);
+                localStorage.setItem(this.nominalSearchKey + '_' + nominal, nominal);
+                if (this.nominals.length > this.maxBanknoteNominals) {
+                    localStorage.removeItem(nominals[0]);
+                }
+                this.getAll();
+            }
+            this.counter = this.nominals.indexOf(nominal);
+            this.next();
+        };
+        this.remove = function (nominal) {
+            if (localStorage.length > 0 && localStorage.key(this.nominalSearchKey + '_' + nominal) !== null) {
+                localStorage.removeItem(this.nominalSearchKey + '_' + nominal);
+                this.counter = this.nominals.indexOf(nominal);
+                this.getAll();
+                this.prev();
+            }
+        };
+        this.removeAll = function () {
+            for (var key in localStorage) {
+                if (key.indexOf(nominalSearchKey) !== -1) {
+                    localStorage.removeItem(key);
+                }
+            }
+        };
+
+        this.next = function () {
+            if (++this.counter > this.nominals.length - 1) {
+                this.counter = 0;
+            }
+            this.current = this.nominals[this.counter];
+            return this.current;
+        };
+        this.current = this.nominals[0];
+        this.prev = function () {
+            if (--this.counter < 0) {
+                this.counter = this.nominals.length - 1;
+            }
+            this.current = this.nominals[this.counter];
+            return this.current;
         };
     }
 })();
